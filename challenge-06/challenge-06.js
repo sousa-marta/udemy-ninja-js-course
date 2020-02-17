e5c10cd982981c98f3ef7955e4da453be4301315
Marta Sousa <marta.sousa@gmail.com>
2020-02-17 20:21:06 -0300

    exe 6 finalizado

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,8 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+let championship = 'Champions League'
+console.log(championship)
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +13,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+let teams = ['time a', 'time b', 'time c', 'time d', 'time e']
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
@@ -32,7 +33,9 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+function showTeamPosition (position){
+    return (position >=1 && position <=5) ? 'O time que está em ' + position + 'º lugar é o ' + teams[position - 1] : 'Não temos a informação do time que está nessa posição.'
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
@@ -44,7 +47,11 @@ função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+let i = 20
+while (i >= 20 && i <=30){
+    console.log (i)
+    i++
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
@@ -58,7 +65,18 @@ Crie uma função chamada `convertToHex`, com as seguintes características:
     a frase:
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+function convertToHex(color){
+    switch(color){
+        case 'red': 
+            console.log('###123')
+            break
+        case 'blue':
+            console.log('###231')
+            break
+        default:
+            console.log('Não temos o equivalente hexadecimal para a cor ' + color)
+    }
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
